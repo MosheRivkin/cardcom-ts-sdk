2ed933db0ae9e60144e823dff3cf2df3eccc091f
MosheRivkin <m0535541179@gmail.com>
2025-08-22 19:31:33 +0300

    chore: bump version to 1.0.2 and update import paths
    - Changed import paths in sdk.ts to use relative paths instead of absolute paths.
    - Modified terminalNumber type in ExternalConfig interface from number to string and converted it back to number in the CardCom class.
    - Removed baseUrl and paths configuration from tsconfig.json.

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,60 +1,60 @@
 
-import { lowProfileCreate } from "@//api/lowProfile/low-profile-create.ts"
-import { lowProfileGetLpResult } from "@//api/lowProfile/low-profile-get-lp-result.ts"
-import { recuringPaymentsGetMuhlafimFile } from "@//api/recuringPayments/recuring-payments-get-muhlafim-file.ts"
-import { recuringPaymentsGetRecurringPayment } from "@//api/recuringPayments/recuring-payments-get-recurring-payment.ts"
-import { recuringPaymentsGetRecurringPaymentHistory } from "@//api/recuringPayments/recuring-payments-get-recurring-payment-history.ts"
-import { recuringPaymentsGetMuhlafimByDate } from "@//api/recuringPayments/recuring-payments-get-muhlafim-by-date.ts"
-import { recuringPaymentsIsBankNumberValid } from "@//api/recuringPayments/recuring-payments-is-bank-number-valid.ts"
-import { recuringPaymentsChangeStatusForHistoryRecurringToIrrevocable } from "@//api/recuringPayments/recuring-payments-change-status-for-history-recurring-to-irrevocable.ts"
-import { accountGetByAccountId } from "@//api/account/account-get-by-account-id.ts"
-import { transactionsListTransactions } from "@//api/transactions/transactions-list-transactions.ts"
-import { transactionsGetTransactionInfoById } from "@//api/transactions/transactions-get-transaction-info-by-id.ts"
-import { transactionsTransaction } from "@//api/transactions/transactions-transaction.ts"
-import { transactionsRefundByTransactionId } from "@//api/transactions/transactions-refund-by-transaction-id.ts"
-import { transactionsSpecialTransactions } from "@//api/transactions/transactions-special-transactions.ts"
-import { transactionsGetTransactionByExternalUniqTran } from "@//api/transactions/transactions-get-transaction-by-external-uniq-tran.ts"
-import { documentsCreateDocument } from "@//api/documents/documents-create-document.ts"
-import { documentsCancelDoc } from "@//api/documents/documents-cancel-doc.ts"
-import { documentsSendAllDocumentsToEmail } from "@//api/documents/documents-send-all-documents-to-email.ts"
-import { documentsGetReport } from "@//api/documents/documents-get-report.ts"
-import { documentsCrossDocument } from "@//api/documents/documents-cross-document.ts"
-import { documentsCreateTaxInvoice } from "@//api/documents/documents-create-tax-invoice.ts"
-import { documentsCreateDocumentUrl } from "@//api/documents/documents-create-document-url.ts"
-import { companyOperationsGetCompanyStatus } from "@//api/companyOperations/company-operations-get-company-status.ts"
-import { companyOperationsAddDocumentToCompany } from "@//api/companyOperations/company-operations-add-document-to-company.ts"
-import { companyOperationsValidateCompanyCreation } from "@//api/companyOperations/company-operations-validate-company-creation.ts"
-import { companyOperationsNewCompany } from "@//api/companyOperations/company-operations-new-company.ts"
-import { companyOperationsCloseCompany } from "@//api/companyOperations/company-operations-close-company.ts"
-import { companyOperationsReOpenCompany } from "@//api/companyOperations/company-operations-re-open-company.ts"
-import { companyOperationsGetCities } from "@//api/companyOperations/company-operations-get-cities.ts"
-import { companyOperationsGetStreets } from "@//api/companyOperations/company-operations-get-streets.ts"
-import { companyOperationsGetCountries } from "@//api/companyOperations/company-operations-get-countries.ts"
-import { companyOperationsGetBanks } from "@//api/companyOperations/company-operations-get-banks.ts"
-import { companyOperationsGetBanksBranches } from "@//api/companyOperations/company-operations-get-banks-branches.ts"
-import { companyOperationsGetMainMcCs } from "@//api/companyOperations/company-operations-get-main-mc-cs.ts"
-import { companyOperationsGetSubMcCs } from "@//api/companyOperations/company-operations-get-sub-mc-cs.ts"
-import { companyOperationsMeagedAddCompany } from "@//api/companyOperations/company-operations-meaged-add-company.ts"
-import { companyOperationsMeagedGetCompanyInfo } from "@//api/companyOperations/company-operations-meaged-get-company-info.ts"
-import { financialCreditCardTransactions } from "@//api/financial/financial-credit-card-transactions.ts"
-import { financialCreditCardTransactionsHalted } from "@//api/financial/financial-credit-card-transactions-halted.ts"
-import { financialFinancialTransactions } from "@//api/financial/financial-financial-transactions.ts"
-import { financialBankDeposites } from "@//api/financial/financial-bank-deposites.ts"
-import { financialGetSlikaInvoices } from "@//api/financial/financial-get-slika-invoices.ts"
-import { tapTransactionsPrepareDevice } from "@//api/tapTransactions/tap-transactions-prepare-device.ts"
-import { tapTransactionsProcessTransaction } from "@//api/tapTransactions/tap-transactions-process-transaction.ts"
-import { tapTransactionsGetTransactions } from "@//api/tapTransactions/tap-transactions-get-transactions.ts"
-import { tapTransactionsGetDevices } from "@//api/tapTransactions/tap-transactions-get-devices.ts"
+import { lowProfileCreate } from "./api/lowProfile/low-profile-create.ts"
+import { lowProfileGetLpResult } from "./api/lowProfile/low-profile-get-lp-result.ts"
+import { recuringPaymentsGetMuhlafimFile } from "./api/recuringPayments/recuring-payments-get-muhlafim-file.ts"
+import { recuringPaymentsGetRecurringPayment } from "./api/recuringPayments/recuring-payments-get-recurring-payment.ts"
+import { recuringPaymentsGetRecurringPaymentHistory } from "./api/recuringPayments/recuring-payments-get-recurring-payment-history.ts"
+import { recuringPaymentsGetMuhlafimByDate } from "./api/recuringPayments/recuring-payments-get-muhlafim-by-date.ts"
+import { recuringPaymentsIsBankNumberValid } from "./api/recuringPayments/recuring-payments-is-bank-number-valid.ts"
+import { recuringPaymentsChangeStatusForHistoryRecurringToIrrevocable } from "./api/recuringPayments/recuring-payments-change-status-for-history-recurring-to-irrevocable.ts"
+import { accountGetByAccountId } from "./api/account/account-get-by-account-id.ts"
+import { transactionsListTransactions } from "./api/transactions/transactions-list-transactions.ts"
+import { transactionsGetTransactionInfoById } from "./api/transactions/transactions-get-transaction-info-by-id.ts"
+import { transactionsTransaction } from "./api/transactions/transactions-transaction.ts"
+import { transactionsRefundByTransactionId } from "./api/transactions/transactions-refund-by-transaction-id.ts"
+import { transactionsSpecialTransactions } from "./api/transactions/transactions-special-transactions.ts"
+import { transactionsGetTransactionByExternalUniqTran } from "./api/transactions/transactions-get-transaction-by-external-uniq-tran.ts"
+import { documentsCreateDocument } from "./api/documents/documents-create-document.ts"
+import { documentsCancelDoc } from "./api/documents/documents-cancel-doc.ts"
+import { documentsSendAllDocumentsToEmail } from "./api/documents/documents-send-all-documents-to-email.ts"
+import { documentsGetReport } from "./api/documents/documents-get-report.ts"
+import { documentsCrossDocument } from "./api/documents/documents-cross-document.ts"
+import { documentsCreateTaxInvoice } from "./api/documents/documents-create-tax-invoice.ts"
+import { documentsCreateDocumentUrl } from "./api/documents/documents-create-document-url.ts"
+import { companyOperationsGetCompanyStatus } from "./api/companyOperations/company-operations-get-company-status.ts"
+import { companyOperationsAddDocumentToCompany } from "./api/companyOperations/company-operations-add-document-to-company.ts"
+import { companyOperationsValidateCompanyCreation } from "./api/companyOperations/company-operations-validate-company-creation.ts"
+import { companyOperationsNewCompany } from "./api/companyOperations/company-operations-new-company.ts"
+import { companyOperationsCloseCompany } from "./api/companyOperations/company-operations-close-company.ts"
+import { companyOperationsReOpenCompany } from "./api/companyOperations/company-operations-re-open-company.ts"
+import { companyOperationsGetCities } from "./api/companyOperations/company-operations-get-cities.ts"
+import { companyOperationsGetStreets } from "./api/companyOperations/company-operations-get-streets.ts"
+import { companyOperationsGetCountries } from "./api/companyOperations/company-operations-get-countries.ts"
+import { companyOperationsGetBanks } from "./api/companyOperations/company-operations-get-banks.ts"
+import { companyOperationsGetBanksBranches } from "./api/companyOperations/company-operations-get-banks-branches.ts"
+import { companyOperationsGetMainMcCs } from "./api/companyOperations/company-operations-get-main-mc-cs.ts"
+import { companyOperationsGetSubMcCs } from "./api/companyOperations/company-operations-get-sub-mc-cs.ts"
+import { companyOperationsMeagedAddCompany } from "./api/companyOperations/company-operations-meaged-add-company.ts"
+import { companyOperationsMeagedGetCompanyInfo } from "./api/companyOperations/company-operations-meaged-get-company-info.ts"
+import { financialCreditCardTransactions } from "./api/financial/financial-credit-card-transactions.ts"
+import { financialCreditCardTransactionsHalted } from "./api/financial/financial-credit-card-transactions-halted.ts"
+import { financialFinancialTransactions } from "./api/financial/financial-financial-transactions.ts"
+import { financialBankDeposites } from "./api/financial/financial-bank-deposites.ts"
+import { financialGetSlikaInvoices } from "./api/financial/financial-get-slika-invoices.ts"
+import { tapTransactionsPrepareDevice } from "./api/tapTransactions/tap-transactions-prepare-device.ts"
+import { tapTransactionsProcessTransaction } from "./api/tapTransactions/tap-transactions-process-transaction.ts"
+import { tapTransactionsGetTransactions } from "./api/tapTransactions/tap-transactions-get-transactions.ts"
+import { tapTransactionsGetDevices } from "./api/tapTransactions/tap-transactions-get-devices.ts"
 
 interface ExternalConfig {
     apiUserName: string;
-    terminalNumber: number;
+    terminalNumber: string;
     apiPassword?: string;
     accountId?: string;
 }
 
 type DerivedInternalConfig<C extends ExternalConfig> = {
-    terminalNumber: C["terminalNumber"];
+    terminalNumber: number;
     ApiName: C["apiUserName"];
     apiUserName: C["apiUserName"];
 } & ('apiPassword' extends keyof C ? { apiPassword: C['apiPassword'] } : {})
@@ -73,7 +73,7 @@ class CardCom<C extends ExternalConfig> {
         this.#config = {
             ApiName: config.apiUserName,
             apiUserName: config.apiUserName,
-            terminalNumber: config.terminalNumber,
+            terminalNumber: Number(config.terminalNumber),
             ...(config.apiPassword && { apiPassword: config.apiPassword }),
             ...(config.accountId && { AccountId: config.accountId }),
         } as DerivedInternalConfig<C>;
